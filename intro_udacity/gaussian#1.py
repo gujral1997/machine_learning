55eabc7c57b1219f02f846714118ac958871949a
Ansh Gujral <anshgujral1997@gmail.com>
2017-12-22 04:41:07 +0530

    few changes

--- intro_udacity/gaussian#1.py
+++ intro_udacity/gaussian#1.py
@@ -2,6 +2,6 @@ import numpy as np
 x=np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
 y=np.array([1,1,1,2,2,2])
 from sklearn.naive_bayes import GaussianNB
-clf=GaussianNB()
+clf=GaussianNB() #classifier
 clf.fit(x,y)
-print(clf.predict([[-0.8,-1]]))
+print(clf.predict([[3,-1],[1,1]]))
