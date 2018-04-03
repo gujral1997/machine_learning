f99eba54eabf9e37f1457abad134b389829c8c7f
Ansh Gujral <anshgujral1997@gmail.com>
2018-04-03 22:57:40 +0530

    added and gate tensor code

--- tensorflow/placeholders.py
+++ tensorflow/placeholders.py
@@ -5,4 +5,5 @@ b = tf.placeholder(tf.float32)
 adder_node = a+b
 
 sess = tf.Session()
-print(sess.run(add, {a:[1,3]}))
+
+print(sess.run(adder_node, {a:[1,3], b:[2, 4]}))
---
+++ tensorflow/variables.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+w = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = w*x+b
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+print(sess.run(linear_model, {x:[1,2,3,4]}))
